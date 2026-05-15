41bf65bf281f898a4f17f313f7dab563a4d5b361
alina tiupalova <bondarenko94191@gmail.com>
2026-05-15 07:46:23 +0300

    Added personal information to index.js

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -1,1 +1,15 @@
- 
+import chalk from 'chalk'
+
+let person = {
+    firstName: "Alina",
+    lastName: "Tiupalova",
+    age: 31,
+};
+console.log(person);
+
+let title = 'Senior Software Test Engineer';
+console.log(chalk.greenBright(title));
+
+let company = 'EPAM Systems';
+console.log(chalk.yellowBright(company));
+
