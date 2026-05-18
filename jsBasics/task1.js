565e8585993012de6fbdc6144d48d07df5337dc4
alina tiupalova <bondarenko94191@gmail.com>
2026-05-19 00:11:30 +0300

    Added jsBasics file with 4 nodes

---
+++ jsBasics/task1.js
@@ -0,0 +1,23 @@
+// number 
+let age = 32;
+console.log(age);
+
+// string 
+let name = 'Alina';
+console.log(name);
+
+// Boolean
+let isTester = true;
+console.log(isTester);
+
+// NaN
+let result = 'Alina' / 2;
+console.log(result);
+
+//Undefined
+let underfinedVariables;
+console.log(underfinedVariables);
+
+//Null
+const type = null;
+console.log(type);
---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let firstName1 = 'John';
+let lastName1 = 'Wick';
+let firstName2 = 'Mike';
+let lastName2 = 'Tyson'
+console.log('Hello ' + firstName1 + ' ' + lastName1 + ' ' + 'and ' + firstName2 + ' ' + lastName2 + '.Nice to see you again.');
+console.log(`Hello ${firstName1} ${lastName1} and ${firstName2} ${lastName2}. Nice to see you again.`);
