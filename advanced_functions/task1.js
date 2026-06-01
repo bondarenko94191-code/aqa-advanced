683b78940d9e251a4ba8e289def8c0a84e6377e0
alina tiupalova <bondarenko94191@gmail.com>
2026-06-01 17:25:19 +0300

    Added advanced_functions folder with 3 tasks

---
+++ advanced_functions/task1.js
@@ -0,0 +1,25 @@
+// Завдання 1
+
+// Створіть функцію handleNum яка буде приймати 3 параметри.
+// число
+// Колбек функцію яку треба викликати якщо передане число парне
+// Колбек функцію яку треба викликати якщо передане число непарне
+// Створіть ще дві функції які ви будете передавати у якості колбеків, наприклад handleEven та handleOdd. Кожна з них має виводити просте повідомлення в консоль. Наприклад handleEven буде виводити текст “number is even”, a handleOdd буде виводити текст “number is odd”
+// Викличте функцію handleNum і передайте в якості аргументів довільне число і дві функції які ви створили раніше
+function handleNum(num, evenNum, oddNum) {
+    if (num % 2 === 0) {
+        evenNum(num);
+    } else {
+        oddNum(num);
+    }
+}
+function handleEven(num) {
+    console.log("number is even");
+}
+function handleOdd(num) {
+    console.log("number is odd");
+}
+handleNum(3, handleEven, handleOdd);
+handleNum(99, handleEven, handleOdd);
+handleNum(4, handleEven, handleOdd);
+handleNum(50, handleEven, handleOdd);
---
+++ advanced_functions/task3.js
@@ -0,0 +1,48 @@
+// Завдання 3
+
+// Створіть функцію divide, яка приймає два параметри: numerator і denominator та повертає як результат виконання число отримане від ділення.
+// У функції треба поділити numerator на denominator і повернути результат.
+// Додайте валідацію в функції. У разі,
+// якщо denominator дорівнює 0
+// або хоча б один з аргументів не є числом, викиньте помилку з інформативним повідомленням,
+// Викличте функцію divide з різними значеннями numerator і denominator, включаючи випадок, коли denominator дорівнює 0 або один з аргументів не є числом.
+// Огорніть кожен окремий виклик функції divide в try…catch.Використовуючи блок finally, виведіть повідомлення "Робота завершена" в консоль, навіть якщо помилка виникла або не виникла.
+
+function divide(numerator, denominator) {
+    if (typeof denominator !== "number" || typeof numerator !== "number") {
+        throw new Error("Provided data is invalid");
+
+    }
+    if (denominator === 0) {
+        throw new Error("Division by 0 is impossible");
+    }
+    return numerator / denominator;
+}
+try {
+    console.log(divide(15, 3));
+}
+catch (error) {
+    console.error(error.message);
+}
+finally {
+    console.log("Робота завершена");
+}
+
+// try {
+//     console.log(divide("hello", 3));
+// }
+// catch (error) {
+//     console.error(error.message);
+// }
+// finally {
+//     console.log("Робота завершена");
+// }
+// try {
+//     console.log(divide(15, 0));
+// }
+// catch (error) {
+//     console.error(error.message);
+// }
+// finally {
+//     console.log("Робота завершена");
+// }
